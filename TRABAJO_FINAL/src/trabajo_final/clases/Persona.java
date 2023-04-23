f67551229bcd289ba826ced101e5422f973b1194
flaven <flaviobustinza@gmail.com>
2023-04-23 00:05:54 -0500

    clases persona-usuario

---
+++ TRABAJO_FINAL/src/trabajo_final/clases/Persona.java
@@ -0,0 +1,54 @@
+package trabajo_final.clases;
+
+/**
+ *
+ * @author Acer
+ */
+public class Persona {
+    private int idpersona;
+    private String cnombre;
+    private String capepat;
+    private String capemat;
+    private String cdni;
+
+    public int getIdpersona() {
+        return idpersona;
+    }
+
+    public void setIdpersona(int idpersona) {
+        this.idpersona = idpersona;
+    }
+
+    public String getCnombre() {
+        return cnombre;
+    }
+
+    public void setCnombre(String cnombre) {
+        this.cnombre = cnombre;
+    }
+
+    public String getCapepat() {
+        return capepat;
+    }
+
+    public void setCapepat(String capepat) {
+        this.capepat = capepat;
+    }
+
+    public String getCapemat() {
+        return capemat;
+    }
+
+    public void setCapemat(String capemat) {
+        this.capemat = capemat;
+    }
+
+    public String getCdni() {
+        return cdni;
+    }
+
+    public void setCdni(String cdni) {
+        this.cdni = cdni;
+    }
+    
+}
---
+++ TRABAJO_FINAL/src/trabajo_final/clases/Usuario.java
@@ -0,0 +1,45 @@
+package trabajo_final.clases;
+
+/**
+ *
+ * @author Acer
+ */
+public class Usuario {
+    private int idcodigo ;
+    private String clogin;
+    private String ccontrasena;
+    private Persona idpersona;
+
+    public int getIdcodigo() {
+        return idcodigo;
+    }
+
+    public void setIdcodigo(int idcodigo) {
+        this.idcodigo = idcodigo;
+    }
+
+    public String getClogin() {
+        return clogin;
+    }
+
+    public void setClogin(String clogin) {
+        this.clogin = clogin;
+    }
+
+    public String getCcontrasena() {
+        return ccontrasena;
+    }
+
+    public void setCcontrasena(String ccontrasena) {
+        this.ccontrasena = ccontrasena;
+    }
+
+    public Persona getIdpersona() {
+        return idpersona;
+    }
+
+    public void setIdpersona(Persona idpersona) {
+        this.idpersona = idpersona;
+    }
+        
+}
