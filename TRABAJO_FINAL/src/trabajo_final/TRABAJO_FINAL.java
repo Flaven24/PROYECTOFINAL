eaea76eeaa406a0e3e15bc89fd4c09473883ddfd
flaven <flaviobustinza@gmail.com>
2023-04-22 23:04:01 -0500

    prueba 1

--- TRABAJO_FINAL/src/trabajo_final/TRABAJO_FINAL.java
+++ TRABAJO_FINAL/src/trabajo_final/TRABAJO_FINAL.java
@@ -16,7 +16,10 @@ public class TRABAJO_FINAL {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-// comentarios
+        // comentarios
+
+        System.out.println("hola como estas");
+
     }
     
 }
