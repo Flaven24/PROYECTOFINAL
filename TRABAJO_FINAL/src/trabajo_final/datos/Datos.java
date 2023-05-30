ab9e89c62b18728ac083e6837bdf05e9068371c4
Flaven <flaviobustinza@gmail.com>
2023-05-30 15:36:57 -0500

    Entidad sin Duplicados

--- TRABAJO_FINAL/src/trabajo_final/datos/Datos.java
+++ TRABAJO_FINAL/src/trabajo_final/datos/Datos.java
@@ -1,6 +1,7 @@
 package trabajo_final.datos;
 
 import trabajo_final.util.Nodo;
+import trabajo_final.util.NodoEntidad;
 
 /**
  *
@@ -8,7 +9,7 @@ import trabajo_final.util.Nodo;
  */
 public class Datos {
 
-    public static Nodo nodoEntidad = new Nodo(null);
+    public static NodoEntidad nodoEntidad = new NodoEntidad(null);
     public static Nodo nodoTipoProcedimiento = new Nodo(null);
     public static Nodo nodoFechaForm = new Nodo(null);
     public static Nodo nodoEstadoOrden = new Nodo(null);
